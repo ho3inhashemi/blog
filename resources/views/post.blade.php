eb1627dde703863ff384dd7aa742a02c2281a569
Hossein <ho3inhashemi@gmail.com>
2021-12-28 00:01:10 +0330

    posts & post hard cord

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+    <article>
+        <h1> My First Post </h1>   
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti molestiae voluptas consequuntur rerum iste nihil corporis dicta dignissimos, quis velit! Placeat, quae dicta saepe quasi rerum ducimus pariatur! Officiis.</p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+    <article>
+        <h1><a href="/post">My First Post </a> </h1>   
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti molestiae voluptas consequuntur rerum iste nihil corporis dicta dignissimos, quis velit! Placeat, quae dicta saepe quasi rerum ducimus pariatur! Officiis.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post </a></h1>   
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti molestiae voluptas consequuntur rerum iste nihil corporis dicta dignissimos, quis velit! Placeat, quae dicta saepe quasi rerum ducimus pariatur! Officiis.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post </a></h1>   
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti molestiae voluptas consequuntur rerum iste nihil corporis dicta dignissimos, quis velit! Placeat, quae dicta saepe quasi rerum ducimus pariatur! Officiis.</p>
+    </article>
+</body>
+</html>
