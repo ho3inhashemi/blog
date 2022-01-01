4af815065be68920b8143c1b686c3e894188ec30
Hossein <ho3inhashemi@gmail.com>
2022-01-01 19:20:19 +0330

    add the author route for all of its posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
 </h1>
 
 <p>
-    By {{$post->user->name}} in <a href="categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+    By <a href="/authors/{{$post->author->username}}"> {{$post->Author->name}} </a> in <a href="categories/{{$post->category->slug}}">{{$post->category->name}}</a>
 </p>
 
 <div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -21,15 +22,14 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts' ,[
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with(['category','author'])->get()
         ] );
 
 });
 
 
 
-Route::get('/post/{post:slug}' , function (Post $post){
-    
+Route::get('/post/{post:slug}' , function (Post $post){  
     return view('post',[
         'post' => $post
         ]);
@@ -38,5 +38,11 @@ Route::get('/post/{post:slug}' , function (Post $post){
 Route::get('categories/{category:slug}' , function (Category $category){
     return view('posts', [
         'posts' => $category->posts
+    ]);
+}); 
+
+Route::get('authors/{author:username}' , function (User $author){
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 }); 
