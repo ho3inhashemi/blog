de353ff5ff263a1c6a635db435dcebdba581cd1b
Hossein <ho3inhashemi@gmail.com>
2022-01-02 13:12:30 +0330

    components completely added to the blades

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,45 +1,19 @@
 <x-layout>
         
-    {{-- @foreach ($posts as $post) 
-    <article>
-
-        <h1>
-            <a href="/post/{{ $post->slug }}">
-                {{ $post->title }} 
-            </a>
-        </h1>
-
-        <p>
-            By <a href="/authors/{{$post->author->username}}"> {{$post->Author->name}} </a> in <a href="categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-        </p>
-
-    <div>
-            {{ $post->excerpt }}
-    </div>
-
-    </article>
-@endforeach --}}
-
 @include('_posts-header')
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+    @if($posts->count())
     
-        <x-post-featured-card/>
+        <x-posts-grid :posts="$posts" />
+    
+    @else
 
-    <div class="lg:grid lg:grid-cols-2">
-        <x-post-card/>
-        <x-post-card/>
+        <p class="text-center">No posts yet,Please check back later.</p>
 
+    @endif
 
-    </div>
-
-    <div class="lg:grid lg:grid-cols-3">
-        
-        <x-post-card/>
-        <x-post-card/>
-        <x-post-card/>
-
-    </div>
 </main>
 
 </x-layout>
